a354de0c3776af59393d03175f3010ddbaebbcb9
pratham877 <prathamsonimau2004@gmail.com>
2025-04-13 20:52:57 +0530

    add exceptions

---
+++ src/main/java/io/github/pratham877/student_management_system/student/exception/StudentValidationException.java
@@ -0,0 +1,7 @@
+package io.github.pratham877.student_management_system.student.exception;
+
+public class StudentValidationException extends RuntimeException {
+    public StudentValidationException(String message) {
+        super(message);
+    }
+}
