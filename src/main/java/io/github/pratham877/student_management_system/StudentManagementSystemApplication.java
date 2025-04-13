01fa87b76f03715a0c5486b4d4cf189221abd6da
pratham877 <prathamsonimau2004@gmail.com>
2025-04-13 13:26:48 +0530

    initial commit

--- src/main/java/io/github/pratham877/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/io/github/pratham877/student_management_system/StudentManagementSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
 
 }
